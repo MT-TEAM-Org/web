e73908b07979cb382940430f711ca806f8bfa375
jun <znznun@gmail.com>
2025-01-20 02:11:04 +0900

    update

--- src/app/(route)/(community)/e-sports/layout.tsx
+++ src/app/(route)/(community)/e-sports/layout.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import Banner from "../_components/banner";
+import Banner from "../_components/Banner";
 import Sidebar from "../_components/Sidebar";
 
 export const metadata = {
