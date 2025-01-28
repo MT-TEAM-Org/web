f83ad51dbf672587d3a43e928ee75ba03aa46a5d
wlrnjs <tjwlrnjs7336@icloud.com>
2025-01-28 17:26:50 +0900

    fix: 기존 스타일링 수정

--- src/app/(route)/(community)/customer/_components/TableWithPagination.tsx
+++ src/app/(route)/(community)/customer/_components/TableWithPagination.tsx
@@ -1,61 +1,87 @@
-import Double_arrow_left from '@/app/_components/icon/Double_arrow_left';
-import Arrow_left from '@/app/_components/icon/Arrow_left';
-import Arrow_right from '@/app/_components/icon/Arrow_right';
-import Double_arrow_right from '@/app/_components/icon/Double_arrow_right';
-import React from 'react';
+import Double_arrow_left from "@/app/_components/icon/Double_arrow_left";
+import Arrow_left from "@/app/_components/icon/Arrow_left";
+import Arrow_right from "@/app/_components/icon/Arrow_right";
+import Double_arrow_right from "@/app/_components/icon/Double_arrow_right";
+import React from "react";
 
 const TableWithPagination = () => {
+  const arrowBtnStyle =
+    "w-[32px] h-[32px] flex gap-[10px] items-center justify-center rounded-[5px] p-[9px] border border-gray-300";
+
   const rows = Array.from({ length: 10 }, (_, index) => ({
     number: 10 - index,
-    content: '제목입니다 제목입니다 제목입니다 제목입니다 제목입니다 제목입니다 제목입니다',
-    date: '2025.02.03',
+    content:
+      "제목입니다 제목입니다 제목입니다 제목입니다 제목입니다 제목입니다 제목입니다",
+    date: "2025.02.03",
   }));
 
   return (
-    <div className="w-full max-w-[1200px] mx-auto">
-      <table className="w-full border-collapse border-gray-300">
-        <thead>
-          <tr className="bg-[#EEEEEE]">
-            <th className="w-[160px] py-2 px-4 text-center">번호</th>
-            <th className="w-[880px] py-2 px-4 text-center">공지내용</th>
-            <th className="w-[160px] py-2 px-4 text-center">날짜</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows.map((row) => (
-            <tr key={row.number} className="h-[48px]">
-              <td className="border-b border-gray-300 py-2 px-4 text-center text-[#424242] text-[16px] leading-[28px] font-[400]">{row.number}</td>
-              <td className="border-b border-gray-300 py-2 px-4 text-[#424242] text-[16px] leading-[28px] font-[400]">{row.content}</td>
-              <td className="border-b border-gray-300 py-2 px-4 text-center text-[#424242] text-[16px] leading-[28px] font-[400]">{row.date}</td>
+    <div className="min-w-[1200px] w-full min-h-[584px] h-auto flex gap-6 flex-col">
+      <div>
+        <table className="w-full min-h-[528px] h-auto">
+          <thead className="w-[1200px] h-[48px] border-[#424242] border-t-[1px] flex">
+            <tr className="bg-[#EEEEEE]">
+              <th className="w-[160px] h-auto py-3 px-4 font-[500] text-[16px] leading-7 text-center">
+                번호
+              </th>
+              <th className="w-[880px] h-auto py-3 px-4 font-[500] text-[16px] leading-7 text-center">
+                공지내용
+              </th>
+              <th className="w-[160px] h-auto py-3 px-4 font-[500] text-[16px] leading-7 text-center">
+                날짜
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {rows.map((row) => (
+              <tr
+                key={row.number}
+                className="w-[1200px] h-[48px] flex align-center justify-center items-center"
+              >
+                <td className="w-[160px] h-[48px] border-b border-gray-300 p-4 flex gap-[10px] justify-center items-center text-[#424242] text-[16px] leading-7 font-[400]">
+                  {row.number}
+                </td>
+                <td className="w-[880px] h-[48px] border-b border-gray-300 p-4 flex gap-[10px] justify-start items-center text-[#424242] text-[16px] leading-7 font-[400]">
+                  {row.content}
+                </td>
+                <td className="w-[160px] h-[48px] border-b border-gray-300 p-4 flex gap-[10px] justify-center items-center text-[#424242] text-[16px] leading-7 font-[400]">
+                  {row.date}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
       {/* 페이지네이션 */}
-      <div className="flex justify-center items-center mt-4 gap-2">
-        <button className="flex items-center justify-center w-[32px] h-[32px] rounded-md border border-gray-300">
-          <Double_arrow_left />
-        </button>
-        <button className="flex items-center justify-center w-[32px] h-[32px] rounded-md border border-gray-300">
-          <Arrow_left />
-        </button>
-        <div className='flex gap-2 items-center text-center'>
-          {Array.from({ length: 5 }, (_, index) => (
-          <button
-            key={index}
-            className='w-[32px] h-[32px] rounded-md px-3 py-1 border border-gray-300'
-          >
-            {index + 1}
+      <div className="min-w-[352px] w-auto min-h-[32px] min-auto flex justify-center items-center gap-2 mb-[37px]">
+        <div className="min-w-[72px] w-auto min-h-[32px] h-auto flex gap-2">
+          <button className={arrowBtnStyle}>
+            <Double_arrow_left />
           </button>
-        ))}
+          <button className={arrowBtnStyle}>
+            <Arrow_left />
+          </button>
         </div>
 
-        <button className="flex items-center justify-center w-[32px] h-[32px] rounded-md border border-gray-300">
-          <Arrow_right />
-        </button>
-        <button className="flex items-center justify-center w-[32px] h-[32px] rounded-md border border-gray-300">
-          <Double_arrow_right />
-        </button>
+        <div className="min-w-[192px] w-auto min-h-[32px] h-auto flex gap-2 text-center">
+          {Array.from({ length: 5 }, (_, index) => (
+            <button
+              key={index}
+              className="w-[32px] h-[32px] rounded-[5px] p-[9px] flex text-center items-center justify-center gap-2 border border-gray-300"
+            >
+              {index + 1}
+            </button>
+          ))}
+        </div>
+
+        <div className="min-w-[72px] w-auto min-h-[32px] h-auto flex gap-2">
+          <button className={arrowBtnStyle}>
+            <Arrow_right />
+          </button>
+          <button className={arrowBtnStyle}>
+            <Double_arrow_right />
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/app/(route)/(community)/customer/page.tsx
+++ src/app/(route)/(community)/customer/page.tsx
@@ -1,6 +1,6 @@
-import Customer_pencil from '@/app/_components/icon/Customer_pencil';
-import React from 'react';
-import TableWithPagination from './_components/TableWithPagination';
+import Customer_pencil from "@/app/_components/icon/Customer_pencil";
+import React from "react";
+import TableWithPagination from "./_components/TableWithPagination";
 
 export const metadata = {
   title: "PlayHive 고객센터 페이지",
@@ -9,31 +9,40 @@ export const metadata = {
 
 const page = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      {/* 고객센터 부분 */}
-      <div className="w-full flex justify-start ml-[510px]">
-        <h1 className="text-[28px] leading-[40px] font-[700] mt-[34px] mb-6">고객센터</h1>
+    <div className="flex justify-center items-center gap-6 mt-[34px]">
+      <div className="w-[1200px] min-h-[820px] h-auto flex flex-col gap-6 justify-center items-center">
+        <div className="w-full min-h-[212px] flex flex-col gap-6">
+          <h1 className="text-[28px] leading-[40px] font-[700]">고객센터</h1>
+          <div className="w-full min-h-[72px] flex gap-10 justify-between bg-[#FAFAFA] rounded-[5px] p-4">
+            <div className="min-w-[1008px] w-auto min-h-[28px] flex items-center gap-5">
+              <Customer_pencil />
+              <span className="font-[700] text-[18px] leading-7">
+                궁금한 내용을 1:1 문의하기에 남겨주세요.
+              </span>
+            </div>
+            <button className="min-w-[120px] min-h-[40px] h-auto py-[13px] px-4 flex gap-[10] justify-center rounded-[5px] bg-[#00ADEE] text-[#FFFFFF] text-[14px] font-[700] leading-[14px]">
+              1:1 문의하기
+            </button>
+          </div>
+
+          <div className="w-full min-h-[52px] h-auto flex items-center">
+            <button className="min-w-[300px] w-auto h-[52px] border-t border-r rounded-tl-[5px] rounded-tr-md border-black px-4 py-[18px] flex justify-center gap-[10px] text-center">
+              공지사항
+            </button>
+            <button className="w-[300px] h-[52px] text-[#A6A6A6] font-bold border-b-2 border-[#DBDBDB] px-4 py-2 text-center">
+              개선요청
+            </button>
+            <button className="w-[300px] h-[52px] text-[#A6A6A6] font-bold border-b-2 border-[#DBDBDB] px-4 py-2 text-center">
+              이용약관
+            </button>
+            <button className="w-[300px] h-[52px] text-[#A6A6A6] font-bold border-b-2 border-[#DBDBDB] px-4 py-2 text-center">
+              개인정보 취급방침
+            </button>
+          </div>
+        </div>
+
+        <TableWithPagination />
       </div>
-      <div className="max-w-[1200px] min-h-[72px] flex gap-4 justify-center items-center bg-[#FAFAFA] px-6 py-4 rounded-lg">
-        <Customer_pencil />
-        <span className="text-[18px] font-[700] w-[1008px] text-start mr-[40px]">
-          궁금한 내용을 1:1 문의하기에 남겨주세요.
-        </span>
-        <button className="w-[120px] min-h-[52px] bg-[#00ADEE] rounded-md text-white text-[14px] font-[500]">
-          1:1 문의하기
-        </button>
-      </div>
-      
-      <div className="max-w-[1200px] min-h-[72px] flex items-center my-6">
-        <button className="w-[300px] h-[52px] text-black font-bold border-t border-r rounded-tl-md rounded-tr-md border-black px-4 py-2 text-center">
-          공지사항
-        </button>
-        <button className="w-[300px] h-[52px] text-[#A6A6A6] font-bold border-b-2 border-[#DBDBDB] px-4 py-2 text-center">개선요청</button>
-        <button className="w-[300px] h-[52px] text-[#A6A6A6] font-bold border-b-2 border-[#DBDBDB] px-4 py-2 text-center">이용약관</button>
-        <button className="w-[300px] h-[52px] text-[#A6A6A6] font-bold border-b-2 border-[#DBDBDB] px-4 py-2 text-center">개인정보 취급방침</button>
-      </div>
-      
-      <TableWithPagination />
     </div>
   );
 };
