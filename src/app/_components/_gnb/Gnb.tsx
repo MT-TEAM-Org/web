e19a8e8316f32227b141a6abdde044704f89eff5
jun <znznun@gmail.com>
2025-02-12 01:59:00 +0900

    delete: Gnb안쓰는 타입 제거

--- src/app/_components/_gnb/Gnb.tsx
+++ src/app/_components/_gnb/Gnb.tsx
@@ -4,19 +4,7 @@ import Header from "./Header";
 import Navbar from "./Navbar";
 import Toast from "../Toast";
 
-interface UserData {
-  id: number;
-  email: string;
-  tel: string;
-  nickname: string;
-  role: string;
-  type: string;
-  status: string;
-}
-
 export default function Gnb() {
-  console.log("Gnb rendering", new Error().stack); // 호출 스택 확인
-
   return (
     <>
       <div className="w-full fixed top-0 bg-white shadow-[0px_6px_10px_0px_rgba(0,0,0,0.05)] z-50 h-[120px]">
