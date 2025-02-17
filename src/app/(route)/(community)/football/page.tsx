e24d5819793fafc1b749595f8b5a01cc8f1bcc4c
wlrnjs <tjwlrnjs7336@icloud.com>
2025-02-17 13:15:12 +0900

    chore: 충돌해결

---
+++ src/app/(route)/(community)/football/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import PostItem from "../_components/PostItem";
+import { CommunityToolbar } from "../_components/CommunityToolbar";
+import React from "react";
+
+const Football = () => {
+  return (
+    <div className="flex justify-center bg-[#FAFAFA] mt-3.5">
+      <div className="max-w-[720px] min-h-[120px] rounded-[5px] border-b bg-[#FFFFFF] mx-auto">
+        <CommunityToolbar />
+        <PostItem />
+        {Array.from({ length: 15 }).map((_, index) => (
+          <PostItem key={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Football;
