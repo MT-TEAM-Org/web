fbb3130b81d98eb0b84112c0cba590c703cf6f14
wlrnjs <tjwlrnjs7336@icloud.com>
2025-02-23 03:00:04 +0900

    design: hover 기능 추가

--- src/app/(route)/(community)/news/_components/NewsPostItem.tsx
+++ src/app/(route)/(community)/news/_components/NewsPostItem.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { NewsItemType } from "@/app/_constants/newsItemType";
+import Link from "next/link";
 
 interface NewsPostItemProps {
   newsItem?: NewsItemType;
@@ -13,10 +14,6 @@ const NewsPostItem = ({ newsItem }: NewsPostItemProps) => {
   const router = useRouter();
 
   const updatedImgUrl = newsItem?.thumbImg?.replace("type=w140", "type=w160");
-
-  const handleToPage = () => {
-    router.push(`/news/news-detail/${newsItem?.id}`);
-  };
 
   const changeCategory = (category?: string) => {
     switch (category) {
@@ -71,62 +68,62 @@ const NewsPostItem = ({ newsItem }: NewsPostItemProps) => {
   };
 
   return (
-    <div
-      className="min-w-[720px] min-h-[116px] flex justify-start gap-3 border-b border-[#FAFAFA] p-3 bg-[#FFFFFF] cursor-pointer"
-      onClick={handleToPage}
-    >
-      <div className="w-[160px] h-[92px] rounded-[3.83px] relative">
-        <Image
-          src={updatedImgUrl || "/Empty_news.png"}
-          alt="thumbImg"
-          width={newsItem?.thumbImg ? 160 : 94.39}
-          height={newsItem?.thumbImg ? 92 : 26}
-          className={`
+    <Link href={`/news/news-detail/${newsItem?.id}`}>
+      <div className="min-w-[720px] min-h-[116px] flex justify-start gap-3 border-b border-[#FAFAFA] p-3 bg-[#FFFFFF] cursor-pointer hover:bg-[#F8FDFF]">
+        <div className="w-[160px] h-[92px] rounded-[3.83px] relative">
+          <Image
+            src={updatedImgUrl || "/Empty_news.png"}
+            alt="thumbImg"
+            width={newsItem?.thumbImg ? 160 : 94.39}
+            height={newsItem?.thumbImg ? 92 : 26}
+            className={`
             ${
               newsItem?.thumbImg
                 ? "w-full h-full object-cover rounded-[5px] gap-[10px]"
                 : "absolute top-[33.5px] left-[33.05px] gap-[3.24px] rounded-[3.83px]"
             }
           `}
-        />
-      </div>
+          />
+        </div>
 
-      <div className="w-[524px] h-auto min-h-[90px] flex flex-col gap-1">
-        <div className="w-[524px] h-auto min-h-[24px] flex gap-[2px] text-start items-center justify-start">
-          <h1 className="font-bold text-[16px] leading-6 tracking-[-2%] text-[#181818] text-ellipsis overflow-hidden whitespace-nowrap">
-            {newsItem?.title}
-          </h1>
-          <p className="font-medium text-[14px] leading-5 text-[#00ADEE]">
-            [24]
-          </p>
-          {newNews(newsItem?.postDate) ? (
-            <p className="font-black text-[10px] leading-[18px] align-center text-[#00ADEE]">
-              N
+        <div className="w-[524px] h-auto min-h-[90px] flex flex-col gap-1">
+          <div className="w-[524px] h-auto min-h-[24px] flex gap-[2px] text-start items-center justify-start">
+            <h1 className="font-bold text-[16px] leading-6 tracking-[-2%] text-[#181818] text-ellipsis overflow-hidden whitespace-nowrap">
+              {newsItem?.title}
+            </h1>
+            <p className="font-medium text-[14px] leading-5 text-[#00ADEE]">
+              [24]
             </p>
-          ) : null}
-        </div>
+            {newNews(newsItem?.postDate) ? (
+              <p className="font-black text-[10px] leading-[18px] align-center text-[#00ADEE]">
+                N
+              </p>
+            ) : null}
+          </div>
 
-        <div>
-          <p className="w-[524px] h-[40px] font-medium text-[14px] leading-5 text-[#424242] overflow-hidden line-clamp-2">
-            컨텐츠들어갈부분 컨텐츠들어갈부분 컨텐츠들어갈부분 컨텐츠들어갈부분
-            컨텐츠들어갈부분 컨텐츠들어갈부분 컨텐츠들어갈부분 컨텐츠들어갈부분
-            컨텐츠들어갈부분 컨텐츠들어갈부분 컨텐츠들어갈부분 컨텐츠들어갈부분
-          </p>
-        </div>
+          <div>
+            <p className="w-[524px] h-[40px] font-medium text-[14px] leading-5 text-[#424242] overflow-hidden line-clamp-2">
+              컨텐츠들어갈부분 컨텐츠들어갈부분 컨텐츠들어갈부분
+              컨텐츠들어갈부분 컨텐츠들어갈부분 컨텐츠들어갈부분
+              컨텐츠들어갈부분 컨텐츠들어갈부분 컨텐츠들어갈부분
+              컨텐츠들어갈부분 컨텐츠들어갈부분 컨텐츠들어갈부분
+            </p>
+          </div>
 
-        <div className="flex gap-1">
-          <p className="font-bold text-[12px] leading-[18px] letter-[-2%] text-[#A6A6A6]">
-            {changeCategory(newsItem?.category)}
-          </p>
-          <p className="font-medium text-[12px] leading-[18px] letter-[-2%] text-[#A6A6A6]">
-            {changeDateAgo(newsItem?.postDate)}
-          </p>
-          <p className="font-medium text-[12px] leading-[18px] letter-[-2%] text-[#A6A6A6]">
-            네이버 스포츠 {/* 목 데이터 */}
-          </p>
+          <div className="flex gap-1">
+            <p className="font-bold text-[12px] leading-[18px] letter-[-2%] text-[#A6A6A6]">
+              {changeCategory(newsItem?.category)}
+            </p>
+            <p className="font-medium text-[12px] leading-[18px] letter-[-2%] text-[#A6A6A6]">
+              {changeDateAgo(newsItem?.postDate)}
+            </p>
+            <p className="font-medium text-[12px] leading-[18px] letter-[-2%] text-[#A6A6A6]">
+              네이버 스포츠 {/* 목 데이터 */}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
