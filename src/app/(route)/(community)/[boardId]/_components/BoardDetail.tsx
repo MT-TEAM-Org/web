d037b2eb5c4b48c6afc36f3e01182260ded663ba
GILPYO <rlfvy789@kakao.com>
2025-02-25 12:13:17 +0900

    Feat: 로딩스피너 라이브러리 추가 상세페이지에 적용

--- src/app/(route)/(community)/[boardId]/_components/BoardDetail.tsx
+++ src/app/(route)/(community)/[boardId]/_components/BoardDetail.tsx
@@ -4,6 +4,7 @@ import useGetBoardDetail from "@/_hooks/getBoardDetail";
 import Image from "next/image";
 import { useState } from "react";
 import parse from "html-react-parser";
+import { Spinner } from "@heroui/react";
 
 interface BoardDetailProps {
   boardId: string;
@@ -26,10 +27,6 @@ const BoardDetail = ({ boardId }: BoardDetailProps) => {
     VERIFICATION: "리뷰",
     TIP: "플레이 팁",
   };
-
-  if (isLoading) {
-    return <div>로딩중...</div>;
-  }
 
   const getKoreanBoardType = (type: string) => {
     return boardTypeMap[type] || type;
@@ -73,48 +70,66 @@ const BoardDetail = ({ boardId }: BoardDetailProps) => {
 
   return (
     <div className="flex flex-col gap-[16px] w-[720px] rounded-5px border-b p-[24px] bg-white shadow-[0_0_10px_0_#0000000D]">
-      <div className="w-[672px] border-b ">
-        <h1 className="font-bold text-[18px] leading-[28px] text-[#303030]">
-          {boardDetailData?.data?.title}
-          <div className="py-2 flex justify-between gap-[16px] w-[672px] min-h-[20px] text-gray6">
-            <div className="flex gap-[8px] w-[421px] h-[20px]">
-              <span className="text-[14px] leading-[20px]">
-                {getKoreanBoardType(boardDetailData?.data?.boardType)}
-              </span>
-              <span className="font-medium text-[14px] leading-[20px]">
-                {getKoreanCategoryType(boardDetailData?.data?.categoryType)}
-              </span>
-              <span className="font-medium text-[14px] leading-[20px]">
-                1분 전
-              </span>
-              <span className="text-[14px] leading-[20px]">조회수</span>
-              <span className="font-medium text-[14px] leading-[20px]">
-                {boardDetailData?.data?.viewCount}
-              </span>
-              <span className="text-[14px] leading-[20px]">댓글</span>
-              <span className="font-medium text-[14px] leading-[20px]">
-                {boardDetailData?.data?.commentCount}
-              </span>
-              <span className="text-[14px] leading-[20px]">조회수</span>
-              <span className="font-medium text-[14px] leading-[20px]">
-                {boardDetailData?.data?.recommendCount}
-              </span>
-            </div>
-            <div className="flex justify-end gap-[12px] w-[235px] h-[20px]">
-              <span className="font-medium text-[14px] leading-[20px]">
-                {boardDetailData?.data?.nickname}
-              </span>
-              <span className="font-medium text-[14px] leading-[20px]">
-                IP {maskIP(boardDetailData?.data?.clientIp)}
-              </span>
-            </div>
+      <div className="w-[672px] border-b">
+        {isLoading ? (
+          <div className="py-4">
+            <h1 className="font-bold text-[18px] leading-[28px] text-[#303030] opacity-50 animate-pulse">
+              로딩 중...
+            </h1>
+            <div className="py-2 flex justify-between gap-[16px] w-[672px] min-h-[20px] text-gray6"></div>
           </div>
-        </h1>
+        ) : (
+          <>
+            <h1 className="font-bold text-[18px] leading-[28px] text-[#303030]">
+              {boardDetailData?.data?.title}
+              <div className="py-2 flex justify-between gap-[16px] w-[672px] min-h-[20px] text-gray6">
+                <div className="flex gap-[8px] w-[421px] h-[20px]">
+                  <span className="text-[14px] leading-[20px]">
+                    {getKoreanBoardType(boardDetailData?.data?.boardType)}
+                  </span>
+                  <span className="font-medium text-[14px] leading-[20px]">
+                    {getKoreanCategoryType(boardDetailData?.data?.categoryType)}
+                  </span>
+                  <span className="font-medium text-[14px] leading-[20px]">
+                    1분 전
+                  </span>
+                  <span className="text-[14px] leading-[20px]">조회수</span>
+                  <span className="font-medium text-[14px] leading-[20px]">
+                    {boardDetailData?.data?.viewCount}
+                  </span>
+                  <span className="text-[14px] leading-[20px]">댓글</span>
+                  <span className="font-medium text-[14px] leading-[20px]">
+                    {boardDetailData?.data?.commentCount}
+                  </span>
+                  <span className="text-[14px] leading-[20px]">조회수</span>
+                  <span className="font-medium text-[14px] leading-[20px]">
+                    {boardDetailData?.data?.recommendCount}
+                  </span>
+                </div>
+                <div className="flex justify-end gap-[12px] w-[235px] h-[20px]">
+                  <span className="font-medium text-[14px] leading-[20px]">
+                    {boardDetailData?.data?.nickname}
+                  </span>
+                  <span className="font-medium text-[14px] leading-[20px]">
+                    IP {maskIP(boardDetailData?.data?.clientIp)}
+                  </span>
+                </div>
+              </div>
+            </h1>
+          </>
+        )}
       </div>
       <div className="content flex flex-col gap-[12px] font-medium text-[16px] leading-[24px] text-gray7">
-        {parse(content, options)}
+        {isLoading ? (
+          <div className="flex justify-center items-center min-h-[200px]">
+            <Spinner className="w-10 h-10" />
+          </div>
+        ) : (
+          parse(content, options)
+        )}
       </div>
     </div>
   );
 };
+
 export default BoardDetail;
