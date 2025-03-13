cfa04d2b61ea63515681d7057c269b6a3e47b7d3
GILPYO <rlfvy789@kakao.com>
2025-03-14 01:46:22 +0900

    delete: 이전 Toast.tsx 삭제

--- src/app/_components/_gnb/Gnb.tsx
+++ src/app/_components/_gnb/Gnb.tsx
@@ -11,7 +11,6 @@ export default function Gnb() {
       <div className="w-full fixed top-0 bg-white shadow-[0px_6px_10px_0px_rgba(0,0,0,0.05)] z-50 h-[120px]">
         <Header />
         <Navbar />
-        {/* <ToastPopUp state="success" title="타이틀" message="메시지" size="PC" /> */}
       </div>
       <div className="pt-[120px]"></div>
     </>
