efd78a305b39c37f9870418a2bfa6c175c19efe3
jun <znznun@gmail.com>
2025-04-01 07:59:02 +0900

    feat: useAuthCheck에러 시 엑세스 토큰 삭제

--- src/_hooks/useAuthCheck.ts
+++ src/_hooks/useAuthCheck.ts
@@ -2,6 +2,8 @@
 
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 const fetchAuthCheck = async () => {
   const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}api/me`, {
@@ -14,6 +16,7 @@ const fetchAuthCheck = async () => {
 };
 
 const useAuthCheck = () => {
+  const router = useRouter();
   const getToken = () => {
     if (typeof window !== "undefined") {
       return localStorage.getItem("accessToken");
@@ -21,7 +24,7 @@ const useAuthCheck = () => {
     return null;
   };
 
-  return useQuery({
+  const query = useQuery({
     queryKey: ["authCheck"],
     queryFn: fetchAuthCheck,
     enabled: !!getToken(),
@@ -29,6 +32,15 @@ const useAuthCheck = () => {
     staleTime: 3600000, // 1 hour in milliseconds
     gcTime: 3600000, // 1 hour in milliseconds
   });
+
+  useEffect(() => {
+    if (query.isError && typeof window !== "undefined") {
+      localStorage.removeItem("accessToken");
+      router.push("/");
+    }
+  }, [query.isError]);
+
+  return query;
 };
 
 export default useAuthCheck;
