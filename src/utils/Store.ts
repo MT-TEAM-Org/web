c56e41b59363467b67154aeb4242e3a8f002d44c
jun <znznun@gmail.com>
2025-04-04 16:44:34 +0900

    feat: 로그인/회원가입 코드 리팩토링 및 소셜 로그인 기본 코드 제거

--- src/utils/Store.ts
+++ src/utils/Store.ts
@@ -1,40 +1,4 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
-
-type SocialState = "google" | "naver" | "kakao" | "discord" | "";
-
-interface SocialStateStore {
-  social: SocialState;
-  setSocial: (social: SocialState) => void;
-  resetSocial: () => void;
-}
-
-export const useSocialStore = create<SocialStateStore>()(
-  persist(
-    (set) => ({
-      social: "",
-      setSocial: (social) => set({ social }),
-      resetSocial: () => set({ social: "" }),
-    }),
-    {
-      name: "social-storage",
-    }
-  )
-);
-
-type SignState = "login" | "signup" | "";
-
-interface SignupStateStore {
-  signStateStore: SignState;
-  setSignupStore: () => void;
-  setClearSignupStore: () => void;
-}
-
-export const useSignupStore = create<SignupStateStore>((set) => ({
-  signStateStore: "",
-  setSignupStore: () => set({ signStateStore: "signup" }),
-  setClearSignupStore: () => set({ signStateStore: "" }),
-}));
 
 interface EditableBoardData {
   categoryType: string;
