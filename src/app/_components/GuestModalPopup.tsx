248d5ed941d97dba89e4ecda698f1b3f08c99a1e
jun <znznun@gmail.com>
2025-04-07 19:08:40 +0900

    disign/비로그인 모달 반응형 구현

--- src/app/_components/GuestModalPopup.tsx
+++ src/app/_components/GuestModalPopup.tsx
@@ -4,6 +4,8 @@ import { createPortal } from "react-dom";
 import { useForm } from "react-hook-form";
 import usePostInquiry from "@/_hooks/usePostInquiry";
 import { useToast } from "@/_hooks/useToast";
+import { cn } from "@/utils";
+import CustomIcon from "./IconComponents/Icon";
 
 interface GuestModalPopupProps {
   show: boolean;
@@ -50,26 +52,59 @@ const GuestModalPopup = ({ show, setShow }: GuestModalPopupProps) => {
     >
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="w-[640px] min-h-[604px] h-auto rounded-[10px] p-10 flex gap-6 flex-col bg-white overflow-hidden items-center justify-center"
+        className={cn(
+          "w-[640px] min-h-[604px] h-auto rounded-[10px] p-10 flex gap-6 flex-col bg-white overflow-hidden items-center justify-center",
+          "mobile:h-screen mobile:rounded-none mobile:p-0 mobile:justify-start mobile:gap-0"
+        )}
         onClick={(e) => e.stopPropagation()}
       >
-        <div className="w-full h-[38px] text-center">
+        <div className={cn("w-full h-[38px] text-center", "mobile:hidden")}>
           <h1 className="font-bold text-[24px] leading-[38px] tracking-[-0.04em]">
             1:1 문의하기
           </h1>
         </div>
-        <div className="w-full min-h-[390px] flex gap-3 flex-col">
+        <div
+          className={cn(
+            "hidden",
+            "mobile:flex mobile:justify-between mobile:items-center mobile:h-[48px] mobile:w-full mobile:border-b-1 mobile:border-gray2"
+          )}
+        >
+          <h1
+            className={cn(
+              "hidden",
+              "mobile:block mobile:font-[700] mobile:leading-[26px] mobile:py-[10px] mobile:pl-[16px]"
+            )}
+          >
+            1:1 문의하기
+          </h1>
+          <button
+            className={cn("hidden", "mobile:block mobile:p-[12px]")}
+            onClick={() => setShow(false)}
+          >
+            <CustomIcon icon="CLOSE_X" className="w-[24px] h-[24px]" />
+          </button>
+        </div>
+        <div
+          className={cn(
+            "w-full min-h-[390px] flex gap-3 flex-col",
+            "mobile:p-[16px]"
+          )}
+        >
           <div className="w-full min-h-[72px] flex flex-col gap-1">
             <div className="flex gap-[2px]">
-              <p className="font-medium text-[14px] leading-[22px] tracking-[-0.02em] text-gray7">
+              <label
+                className="font-medium text-[14px] leading-[22px] tracking-[-0.02em] text-gray7"
+                htmlFor="email"
+              >
                 연락 받을 이메일 주소
-              </p>
+              </label>
               <span className="font-medium text-[14px] leading-[22px] tracking-[-0.02em] text-warning">
                 *
               </span>
             </div>
             <input
               type="text"
+              id="email"
               placeholder="이메일을 입력해주세요."
               autoFocus
               className="w-full rounded-[5px] min-h-[46px] border px-4 py-3 border-gray3 bg-white"
@@ -91,16 +126,23 @@ const GuestModalPopup = ({ show, setShow }: GuestModalPopupProps) => {
 
           <div className="w-full min-h-[226px] flex flex-col gap-1">
             <div className="flex gap-[2px]">
-              <p className="font-medium text-[14px] leading-[22px] tracking-[-0.02em] text-gray7">
+              <label
+                htmlFor="content"
+                className="font-medium text-[14px] leading-[22px] tracking-[-0.02em] text-gray7"
+              >
                 문의 내용
-              </p>
+              </label>
               <span className="font-medium text-[14px] leading-[22px] tracking-[-0.02em] text-warning">
                 *
               </span>
             </div>
             <textarea
               placeholder="문의 내용을 입력해주세요. (최대 400자 이내)"
-              className="resize-none w-full rounded-[5px] min-h-[200px] border px-4 py-3 border-gray3 bg-white"
+              id="content"
+              className={cn(
+                "resize-none w-full rounded-[5px] min-h-[200px] border px-4 py-3 border-gray3 bg-white",
+                "mobile:min-h-[240px]"
+              )}
               {...register("content", {
                 required: "5자 이상 입력해주세요",
                 minLength: { value: 5, message: "5자 이상 입력해주세요" },
@@ -118,7 +160,12 @@ const GuestModalPopup = ({ show, setShow }: GuestModalPopupProps) => {
             />
           </div>
 
-          <div className="w-full h-[68px] rounded-[5px] p-3 flex gap-4 bg-gray1 font-medium text-[14px] leading-[22px] tracking-[-0.02em] text-gray6">
+          <div
+            className={cn(
+              "w-full h-[68px] rounded-[5px] p-3 flex gap-4 bg-gray1 font-medium text-[14px] leading-[22px] tracking-[-0.02em] text-gray6",
+              "mobile:h-[90px]"
+            )}
+          >
             <p>
               고객님의 문의글은 순차적으로 응답해드리고 있습니다. 최선을 다하는
               플레이 하이브가 되겠습니다. AM 10:00 - PM 7:00 (주말 및 공휴일
@@ -127,15 +174,26 @@ const GuestModalPopup = ({ show, setShow }: GuestModalPopupProps) => {
           </div>
         </div>
 
-        <div className="w-full h-[48px] flex gap-2 items-center justify-center">
+        <div
+          className={cn(
+            "w-full h-[48px] flex gap-2 items-center justify-center",
+            "mobile:m-[16px] mobile:px-[16px] mobile:fixed mobile:bottom-0"
+          )}
+        >
           <button
             type="button"
-            className={`${buttonStyle} border bg-white border-gray3`}
+            className={cn(
+              `${buttonStyle} border bg-white border-gray3`,
+              "mobile:hidden"
+            )}
             onClick={() => setShow(false)}
           >
             취소
           </button>
-          <button type="submit" className={`${buttonStyle} bg-gra text-white`}>
+          <button
+            type="submit"
+            className={cn(`${buttonStyle} bg-gra text-white`, "mobile:w-full")}
+          >
             문의하기
           </button>
         </div>
