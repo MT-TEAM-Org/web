e25a02ac4e3a2de3b5b7788cef82b64749a0cdb0
jun <znznun@gmail.com>
2025-04-07 21:07:55 +0900

    disign/회원가입 약관 모달 반응형 구현

--- src/app/_components/termsModal/TermsModal.tsx
+++ src/app/_components/termsModal/TermsModal.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { cn } from "@/utils";
 import TermsPersonalText from "./TermsPersonalText";
 import TermsServiceText from "./TermsServiceText";
+import CustomIcon from "../IconComponents/Icon";
 
 interface Show {
   service: boolean;
@@ -37,21 +39,78 @@ const TearmsModal = ({ show, setShow }: TearmsModalProps) => {
       onClick={disabledModal}
     >
       <div
-        className="flex flex-col gap-[24px] p-[40px] w-[640px] h-[600px] rounded-[10px] bg-[#FFFFFF] shadow-lg text-[#000000]"
+        className={cn(
+          "flex flex-col gap-[24px] p-[40px] w-[640px] h-[600px] rounded-[10px] bg-[#FFFFFF] shadow-lg text-[#000000]",
+          "mobile:w-full mobile:h-screen mobile:rounded-none mobile:p-0 mobile:justify-start mobile:gap-0 mobile:overflow-y-scroll"
+        )}
         onClick={(e) => e.stopPropagation()}
       >
-        <div className="flex flex-col items-center gap-[16px]">
-          <h1 className="font-[700] text-[24px] leading-[38px] text-[#000000]">
+        <div
+          className={cn(
+            "flex flex-col items-center gap-[16px]",
+            "mobile:gap-0"
+          )}
+        >
+          <h1
+            className={cn(
+              "font-[700] text-[24px] leading-[38px] text-[#000000]",
+              "mobile:hidden"
+            )}
+          >
             {show.service ? "이용약관" : "개인정보 수집 및 이용 동의"}
           </h1>
-          <div className="overflow-y-auto min-w-[560px] h-[390px] bg-[#FAFAFA] rounded-[10px] p-[32px]">
-            {show.service ? <TermsServiceText /> : <TermsPersonalText />}
+          <div
+            className={cn(
+              "hidden",
+              "mobile:flex mobile:justify-between mobile:items-center mobile:h-[48px] mobile:w-full mobile:border-b-1 mobile:border-gray2"
+            )}
+          >
+            <h1
+              className={cn(
+                "hidden",
+                "mobile:block mobile:font-[700] mobile:leading-[26px] mobile:py-[10px] mobile:pl-[16px]"
+              )}
+            >
+              {show.service ? "이용약관" : "개인정보 수집 및 이용 동의"}
+            </h1>
+            <button
+              className={cn("hidden", "mobile:block mobile:p-[12px]")}
+              onClick={disabledModal}
+            >
+              <CustomIcon icon="CLOSE_X" className="w-[24px] h-[24px]" />
+            </button>
+          </div>
+          <div
+            className={cn(
+              "overflow-y-auto min-w-[560px] h-[390px] bg-[#FAFAFA] rounded-[10px]",
+              "mobile:min-w-full mobile:h-auto mobile:overflow-auto mobile:bg-white"
+            )}
+          >
+            {show.service ? (
+              <div
+                className={cn("p-[32px]", "mobile:p-[16px] mobile:pl-[32px]")}
+              >
+                <TermsServiceText />
+              </div>
+            ) : (
+              <div className={cn("p-[32px]", "mobile:p-[16px]")}>
+                <TermsPersonalText />
+              </div>
+            )}
           </div>
         </div>
-        <div className="flex justify-center">
+        <div
+          className={cn(
+            "flex justify-center",
+            "mobile:w-full mobile:bg-white mobile:p-[16px] mobile:pt-0"
+          )}
+        >
           <button
             type="button"
-            className="flex items-center justify-center w-[160px] h-[52px] rounded-[5px] px-[18px] py-[22px] bg-gra text-[#FFFFFF] text-[16px] font-[700]"
+            className={cn(
+              "flex items-center justify-center w-[160px] h-[52px] rounded-[5px] px-[18px] py-[22px] bg-gra text-[#FFFFFF] text-[16px] font-[700]",
+              "mobile:w-full"
+            )}
             onClick={disabledModal}
           >
             확인
