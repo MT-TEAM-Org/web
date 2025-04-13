f05de659a793e0f8f6cf1adc939b585fc1170a5c
wlrnjs <tjwlrnjs7336@icloud.com>
2025-04-13 14:49:11 +0900

    design/스켈레톤UI추가및에러처리

--- src/app/(route)/mypage/posts/_components/MypagePostEmpty.tsx
+++ src/app/(route)/mypage/posts/_components/MypagePostEmpty.tsx
@@ -1,8 +1,14 @@
 import { LogoWhite } from "@/app/_components/icon/LogoWhite";
 
-const MyPagePostEmpty = () => {
+interface MyPagePostEmptyProps {
+  height?: string;
+}
+
+const MyPagePostEmpty = ({ height = "h-[248px]" }: MyPagePostEmptyProps) => {
   return (
-    <div className="flex items-center justify-center w-full h-[248px] rounded-b-[10px] bg-gray1">
+    <div
+      className={`flex items-center justify-center w-full ${height} rounded-b-[10px] bg-gray1`}
+    >
       <div className="flex flex-col justify-center items-center min-h-[160px] space-y-[16px]">
         <div className="opacity-30">
           <LogoWhite />
