71f5254ec0aca8c185a0dc108d48256201d92ee5
jun <znznun@gmail.com>
2025-04-14 20:39:53 +0900

    feat: useDeleteAccount 탈퇴 후 로그아웃처리 변경

--- src/_hooks/fetcher/mypage/useDeleteAccount.ts
+++ src/_hooks/fetcher/mypage/useDeleteAccount.ts
@@ -5,18 +5,21 @@ import { useMutation } from "@tanstack/react-query";
 import { useQueryClient } from "@tanstack/react-query";
 import { useToast } from "@/_hooks/useToast";
 import { useRouter } from "next/navigation";
+import { useAuthStore } from "@/utils/Store";
 
 const useDeleteAccount = () => {
   const queryClient = useQueryClient();
   const router = useRouter();
   const { success } = useToast();
+  const { logout } = useAuthStore();
 
   return useMutation({
     mutationFn: () => deleteAccount(),
     onSuccess: () => {
       success("회원탈퇴가 완료되었습니다.", "이용해주셔서 감사합니다.");
       localStorage.removeItem("accessToken");
-      queryClient.invalidateQueries({ queryKey: ["authCheck"] });
+      queryClient.refetchQueries({ queryKey: ["authCheck"] });
+      logout();
       router.push("/");
     },
   });
