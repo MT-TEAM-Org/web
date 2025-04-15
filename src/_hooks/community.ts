d59acfe960bdbde670b2bfd6ffcdc4eafaa9106e
jun <znznun@gmail.com>
2025-04-16 03:43:45 +0900

    fix: usePostCommunityContent 토스트 구조분해할당으로 수정

--- src/_hooks/community.ts
+++ src/_hooks/community.ts
@@ -23,12 +23,12 @@ const postCommunitycontent = async (data: CommunityData) => {
 
 const usePostCommunityContent = () => {
   const router = useRouter();
-  const toast = useToast();
+  const { success, error: toastError } = useToast();
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (data: CommunityData) => postCommunitycontent(data),
     onSuccess: (response) => {
-      toast.success("게시글 작성을 완료했습니다.", "");
+      success("게시글 작성을 완료했습니다.", "");
       queryClient.invalidateQueries({ queryKey: ["myPostList"] });
       const boardType = response?.data?.boardType.toLowerCase();
       const categoryType = response?.data?.categoryType;
@@ -43,9 +43,9 @@ const usePostCommunityContent = () => {
       if (axios.isAxiosError(error) && error.response) {
         const errorMessage =
           error.response.data.message || "알 수 없는 오류가 발생했습니다.";
-        toast.error("게시글 등록 실패", errorMessage);
+        toastError("게시글 등록 실패", errorMessage);
       } else {
-        toast.error("게시글 등록 실패", "게시글 등록 중 오류가 발생했습니다.");
+        toastError("게시글 등록 실패", "게시글 등록 중 오류가 발생했습니다.");
       }
     },
   });
