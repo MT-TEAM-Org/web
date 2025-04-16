1a9ae8031ff9d3b3b7f2901851776de1d8cae3be
jun <znznun@gmail.com>
2025-04-16 09:23:41 +0900

    update

--- src/_hooks/fetcher/sign/useTokenRefreshOnNavigation.ts
+++ src/_hooks/fetcher/sign/useTokenRefreshOnNavigation.ts
@@ -33,8 +33,9 @@ const useTokenRefreshOnNavigation = () => {
       const accessToken = localStorage.getItem("accessToken");
       if (!accessToken) return;
       const payload = parseJwt(accessToken);
+      console.log(payload.exp * 1000);
       if (!payload.exp) return;
-      const isExpired = payload.exp * 1000 - Date.now() < 5 * 60 * 1000;
+      const isExpired = payload.exp * 1000 - Date.now() < 5 * 60 * 1000; // 5 minutes before expiration
       if (isExpired) {
         reissue();
       }
