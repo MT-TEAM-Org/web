17b027e3e29b323e4a76861a31b6f1a6201f0bae
Jedonge <jedonge1995@gmail.com>
2025-04-19 20:05:40 +0900

    feat: 이메일템플릿 추가

--- src/_hooks/fetcher/sign/useSendVerification.ts
+++ src/_hooks/fetcher/sign/useSendVerification.ts
@@ -6,10 +6,8 @@ import { useToast } from "@/_hooks/useToast";
 
 const useSendVerification = () => {
   const { success, error } = useToast();
-
   return useMutation({
     mutationFn: (email: string) => sendVerification(email),
-
     onSuccess: () => {
       success("인증 메일이 발송되었습니다.", "이메일을 확인해주세요.");
     },
@@ -20,23 +18,3 @@ const useSendVerification = () => {
 };
 
 export default useSendVerification;
-// "use client";
-
-// import sendVerification from "@/services/sign/sendVerification";
-// import { useMutation } from "@tanstack/react-query";
-// import { useToast } from "@/_hooks/useToast";
-
-// const useSendVerification = () => {
-//   const { success, error } = useToast();
-//   return useMutation({
-//     mutationFn: (email: string) => sendVerification(email),
-//     onSuccess: () => {
-//       success("인증 메일이 발송되었습니다.", "이메일을 확인해주세요.");
-//     },
-//     onError: () => {
-//       error("인증 메일 발송에 실패했습니다.", "");
-//     },
-//   });
-// };
-
-// export default useSendVerification;
