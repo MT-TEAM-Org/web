327b76d79d5b270cc1e2c38586e0de8e5b1d2b4e
Jedonge <jedonge1995@gmail.com>
2025-04-19 19:56:04 +0900

    feat: 이메일 인증 템플릿 추가

--- src/services/sign/sendVerification.ts
+++ src/services/sign/sendVerification.ts
@@ -2,27 +2,15 @@ import axios from "axios";
 import generateVerificationEmail from "./emailTemplate/emailTemlate";
 
 const sendVerification = async (email: string) => {
-  const htmlTemplate = generateVerificationEmail(email); // {code} 비워진 상태
+  const htmlTemplate = generateVerificationEmail(email);
   const response = await axios.post(
     `${process.env.NEXT_PUBLIC_API_URL}/api/certification/send`,
     {
       email,
-      html: htmlTemplate, // {code}가 그대로 있는 HTML을 보냄
+      html: htmlTemplate,
     }
   );
   return response.data;
 };
 
-export default sendVerification; // import axios from "axios";
-
-// const sendVerification = async (email: string) => {
-//   const response = await axios.post(
-//     `${process.env.NEXT_PUBLIC_API_URL}api/certification/send`,
-//     {
-//       email,
-//     }
-//   );
-//   return response.data;
-// };
-
-// export default sendVerification;
+export default sendVerification;
