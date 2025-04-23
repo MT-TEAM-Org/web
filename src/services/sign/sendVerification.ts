6afb9fb6fa6696e1de9926e0d7c8b75cf919defb
Jedonge <jedonge1995@gmail.com>
2025-04-24 03:04:37 +0900

    edit: 이메일템플릿 삭제 및 senverification api 수정

--- src/services/sign/sendVerification.ts
+++ src/services/sign/sendVerification.ts
@@ -1,13 +1,10 @@
 import axios from "axios";
-import generateVerificationEmail from "./emailTemplate/emailTemplate";
 
 const sendVerification = async (email: string) => {
-  const htmlTemplate = generateVerificationEmail(email);
   const response = await axios.post(
     `${process.env.NEXT_PUBLIC_API_URL}api/certification/send`,
     {
       email,
-      html: htmlTemplate,
     }
   );
   return response.data;
