6471ed183170720e252abf33403a1915c9fbe644
wlrnjs <tjwlrnjs7336@icloud.com>
2025-04-25 01:40:01 +0900

    feat:서비스소개애니메이션수정

--- src/app/(route)/matchBroadcast/_components/matchGnb/MatchMobileGnb.tsx
+++ src/app/(route)/matchBroadcast/_components/matchGnb/MatchMobileGnb.tsx
@@ -6,7 +6,6 @@ import { cn } from "@/utils";
 import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
 import MatchMobileGnbModal from "./MatchMobileGnbModal";
-import Link from "next/link";
 
 interface MobileGnbProps {
   type: "match" | "service";
@@ -31,6 +30,14 @@ const MatchMobileGnb = ({ type }: MobileGnbProps) => {
   const onClose = () => setIsModalOpen(false);
   const onOpen = () => setIsModalOpen(true);
 
+  const handleToRouting = () => {
+    if (type === "match") {
+      router.push("/");
+    } else {
+      router.back();
+    }
+  };
+
   return (
     <div
       className={cn(
@@ -39,14 +46,15 @@ const MatchMobileGnb = ({ type }: MobileGnbProps) => {
         "pc:hidden tablet:hidden"
       )}
     >
-      <Link href={"/"}>
-        <div className="w-[48px] h-[48px] flex items-center justify-center cursor-pointer">
-          <CustomIcon
-            icon="MOBILE_ARROW_LEFT"
-            className="w-[18px] h-[18px] text-white"
-          />
-        </div>
-      </Link>
+      <div
+        onClick={handleToRouting}
+        className="w-[48px] h-[48px] flex items-center justify-center cursor-pointer"
+      >
+        <CustomIcon
+          icon="MOBILE_ARROW_LEFT"
+          className="w-[18px] h-[18px] text-white"
+        />
+      </div>
 
       {type === "match" ? (
         <div className="w-full flex items-center justify-between">
--- src/app/(route)/service-introduction/_components/ServiceAnimation.tsx
+++ src/app/(route)/service-introduction/_components/ServiceAnimation.tsx
@@ -4,31 +4,25 @@ import Image from "next/image";
 const ServiceAnimation = () => {
   return (
     <div className="absolute overflow-hidden w-full h-[140px]">
-      <div className="flex w-[8528px] scroll-x-animation gap-[60px]">
-        <Image
-          src="/Service_animation1.png"
-          alt="서비스소개 이미지"
-          width={1227}
-          height={140}
-        />
-        <Image
-          src="/Service_animation2.png"
-          alt="서비스소개 이미지"
-          width={2134}
-          height={140}
-        />
-        <Image
-          src="/Service_animation1.png"
-          alt="서비스소개 이미지"
-          width={1227}
-          height={140}
-        />
-        <Image
-          src="/Service_animation2.png"
-          alt="서비스소개 이미지"
-          width={2134}
-          height={140}
-        />
+      <div className="flex gap-[60px] w-max animate-marquee">
+        {[...Array(6)].map((_, index) => (
+          <React.Fragment key={index}>
+            <Image
+              src="/Service_animation1.png"
+              alt="서비스소개 이미지"
+              width={1227}
+              height={140}
+              priority
+            />
+            <Image
+              src="/Service_animation2.png"
+              alt="서비스소개 이미지"
+              width={2134}
+              height={140}
+              priority
+            />
+          </React.Fragment>
+        ))}
       </div>
     </div>
   );
