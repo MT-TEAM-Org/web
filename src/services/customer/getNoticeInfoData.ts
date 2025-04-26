eaf8efe66653e6c2aefda683c7692c3a558dfa23
wlrnjs <tjwlrnjs7336@icloud.com>
2025-04-27 05:12:47 +0900

    feat:조회수중복방지추가

--- src/services/customer/getNoticeInfoData.ts
+++ src/services/customer/getNoticeInfoData.ts
@@ -3,11 +3,13 @@ import axios from "axios";
 interface getNoticeInfoDataProps {
   id: number;
   token?: string;
+  openGraph?: boolean;
 }
 
-const getNoticeInfoData = async ({id, token}: getNoticeInfoDataProps) => {
+const getNoticeInfoData = async ({id, token, openGraph}: getNoticeInfoDataProps) => {
   const accessToken = token || (typeof window !== "undefined" ? localStorage.getItem('accessToken') : "");
-  const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}api/notice/${id}`, {
+  const url = `${process.env.NEXT_PUBLIC_API_URL}api/notice/${id}${openGraph ? "?openGraph=true" : ""}`;
+  const response = await axios.get(url, {
     headers: {
       Authorization: accessToken ? `${accessToken}` : '',
     }
