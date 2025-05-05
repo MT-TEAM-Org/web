f58a6bea605eda0daf55251924cc220e5e285b43
GILPYO <rlfvy789@kakao.com>
2025-05-05 23:55:53 +0900

    fix: 오픈그래프 불린 값 테스트

--- src/services/board/getBoardDetail.ts
+++ src/services/board/getBoardDetail.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-const getBoardDetail = async (boardId: string) => {
+const getBoardDetail = async (boardId: string, openGraph: boolean) => {
   const response = await axios.get(
-    `${process.env.NEXT_PUBLIC_API_URL}api/board/${boardId}`
+    `${process.env.NEXT_PUBLIC_API_URL}api/board/${boardId}${
+      openGraph ? "?openGraph=true" : ""
+    }`
   );
   return response.data;
 };
