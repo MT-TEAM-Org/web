f29f53d378341601a117c41a9770553c888d974b
wlrnjs <tjwlrnjs7336@icloud.com>
2025-05-15 22:52:07 +0900

    임시커밋

--- src/app/(route)/main/_components/EventItem.tsx
+++ src/app/(route)/main/_components/EventItem.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import CustomIcon from "@/app/_components/IconComponents/Icon";
-import { cn } from "@/utils";
 
 export interface GameEventData {
   description: string;
