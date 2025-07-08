2dd5f4d2eeb15cb8f399015d88fbc8caa4e66409
wlrnjs <tjwlrnjs7336@icloud.com>
2025-07-08 15:48:54 +0900

    feat: 뒤로가기 버튼 기능 추가

--- src/app/(admin)/admin-inquiries/[inquiriesId]/page.tsx
+++ src/app/(admin)/admin-inquiries/[inquiriesId]/page.tsx
@@ -2,12 +2,15 @@ import Icon from "@/app/_components/IconComponents";
 import React from "react";
 import InquiriesMeta from "./_components/InquiriesMeta";
 import UserInquiriesList from "./_components/UserInquiriesList";
+import Link from "next/link";
 
 const Page = () => {
   return (
     <div className="w-full min-h-screen flex flex-col gap-4">
       <div className="flex gap-2 items-center justify-start">
-        <Icon icon="BACK_ARROW" />
+        <Link href="/admin-inquiries">
+          <Icon icon="BACK_ARROW" />
+        </Link>
         <h1 className="font-bold text-[24px] leading-38px] tracking-[-0.04em] text-black">
           문의 내역 상세
         </h1>
