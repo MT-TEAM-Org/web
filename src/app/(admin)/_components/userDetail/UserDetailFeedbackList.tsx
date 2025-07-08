9dee09d94248b34f720243b3bcfd84b6e0b5c098
wlrnjs <tjwlrnjs7336@icloud.com>
2025-07-09 00:44:00 +0900

    refactor: 상세페이지 컴포넌트 통합

--- src/app/(admin)/_components/userDetail/UserDetailFeedbackList.tsx
+++ src/app/(admin)/_components/userDetail/UserDetailFeedbackList.tsx
@@ -3,22 +3,32 @@
 import DetailTable from "@/app/(admin)/_components/DetailTable";
 import React from "react";
 
-const UserInquiriesList = () => {
+interface UserImprovementListProps {
+  totalCount: string;
+  type: "improvement" | "inquiry";
+  title: string;
+}
+
+const UserDetailFeedbackList = ({
+  totalCount,
+  type,
+  title,
+}: UserImprovementListProps) => {
   return (
     <div className="w-1/2 flex flex-col gap-2">
       <div className="flex gap-2 items-center">
         <h3 className="font-bold text-[18px] leading-7 tracking-[-0.04em] text-black">
-          해당 유저의 문의 내역
+          {title}
         </h3>
         <p className="font-bold text-[16px] leading-7 tracking-[-0.02em] text-gray7">
-          총 165건
+          총 {totalCount}건
         </p>
       </div>
       <div>
-        <DetailTable type="inquiry" isList={false} />
+        <DetailTable type={type} isList={false} />
       </div>
     </div>
   );
 };
 
-export default UserInquiriesList;
+export default UserDetailFeedbackList;
