892a80bcea579a820be3dc49aa62f6ae580bbef1
wlrnjs <tjwlrnjs7336@icloud.com>
2025-07-09 10:10:54 +0900

    feat: 모달 임시 커밋

---
+++ src/app/(admin)/_components/modal/PostNoticeModal.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { cn } from "@/utils";
+import React from "react";
+import { createPortal } from "react-dom";
+
+interface PostNoticeModalProps {
+  show: boolean;
+  setShow: (show: boolean) => void;
+}
+
+const boxStyle = "w-full flex items-center border-b border-gray2";
+const labelStyle =
+  "w-[100px] h-full px-3 py-2 bg-gray1 font-bold text-[14px] leading-5 text-gray8 flex items-center justify-center";
+const inputStyle =
+  "flex-1 px-4 py-2 bg-white text-[14px] leading-5 text-gray8 border border-x-0 border-gray2";
+
+const PostNoticeModal = ({ show, setShow }: PostNoticeModalProps) => {
+  return createPortal(
+    <div
+      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80 z-50"
+      onClick={() => setShow(false)}
+    >
+      <form
+        action=""
+        className="w-[800px] min-h-[670px] rounded-[10px] p-6 bg-white flex flex-col items-center justify-center gap-6"
+      >
+        <h3 className="font-bold text-[24px] leading-[38px] tracking-[-0.04em] text-black">
+          공지사항 등록
+        </h3>
+        <div className="w-full h-auto border border-gray2">
+          <div className={cn(boxStyle, "h-[40px]")}>
+            <p className={labelStyle}>작성자</p>
+            <p className="flex-1 h-full px-4 py-2 bg-white text-[14px] leading-5 text-gray8">
+              플레이하이브 관리자
+            </p>
+          </div>
+          <div className={cn(boxStyle, "h-[56px]")}>
+            <p className={labelStyle}>제목</p>
+            <div className={inputStyle}>
+              <input
+                type="text"
+                placeholder="내용을 입력해주세요"
+                className="w-full h-[40px] rounded-[5px] border p-3 bg-white border-gray3"
+              />
+            </div>
+          </div>
+          <div className={cn(boxStyle, "h-[56px]")}>
+            <p className={labelStyle}>첨부링크</p>
+            <div className={inputStyle}>
+              <input
+                type="text"
+                placeholder="내용을 입력해주세요"
+                className="w-full h-[40px] rounded-[5px] border p-3 bg-white border-gray3"
+              />
+            </div>
+          </div>
+          <div className={cn(boxStyle, "h-full")}>
+            <p className={cn(labelStyle, "h-[344px]")}>내용</p>
+            <div className="flex-1 h-[344px] px-4 py-2 bg-white flex flex-col items-center justify-center">
+              <div className="w-full h-[44px] bg-gray5 flex items-center justify-center">
+                팁탭 들어갈 부분
+              </div>
+              <textarea
+                name=""
+                id=""
+                className="w-full flex-1 h-[284px] rounded-[5px] rounded-t-none border border-t-0 border-gray3 px-4 py-3 resize-none text-[14px] leading-[22px] tracking-[-0.02em] placeholder:text-gray5 placeholder:font-normal"
+                placeholder="내용을 입력해주세요"
+              ></textarea>
+            </div>
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="w-[120px] h-[40px] rounded-[5px] px-4 py-[13px] flex items-center justify-center bg-gra font-bold text-[14px] text-white"
+        >
+          등록
+        </button>
+      </form>
+    </div>,
+    document.body
+  );
+};
+
+export default PostNoticeModal;
---
+++ src/app/(admin)/dashBoard/notices/page.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { cn } from "@/utils";
+import React, { useState } from "react";
+import PostNoticeModal from "../../_components/modal/PostNoticeModal";
+
+const buttonStyle =
+  "w-[120px] h-[40px] flex items-center justify-center rounded-[5px] px-4 py-[13px] font-bold text-[14px]";
+
+const Page = () => {
+  const [show, setShow] = useState(false);
+
+  const button = [
+    {
+      name: "전체 삭제",
+      value: "deleteAll",
+      style: "bg-white border border-gray3 hover:bg-gray1",
+    },
+    {
+      name: "삭제",
+      value: "delete",
+      style: "bg-white border border-gray3 hover:bg-gray1",
+    },
+    {
+      name: "공지 등록",
+      value: "register",
+      style: "bg-Primary text-white hover:bg-primary/80",
+      onClick: () => setShow(true),
+    },
+  ];
+
+  return (
+    <div>
+      <div className="w-full flex justify-between items-center">
+        <div className="flex gap-2 items-center">
+          <h1 className="font-bold text-[20px] leading-[36px] tracking-[-0.02em] text-black">
+            공지 내역
+          </h1>
+          <p className="font-bold text-[16px] leading-[24px] tracking-[-0.02em] text-gray7">
+            총 165건
+          </p>
+        </div>
+        <div className="flex gap-2">
+          {button.map((button) => (
+            <button
+              key={button.value}
+              className={cn(buttonStyle, button.style)}
+              onClick={button.onClick}
+            >
+              {button.name}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className="w-full bg-gray1">표 들어갈 부분</div>
+
+      <PostNoticeModal show={show} setShow={setShow} />
+    </div>
+  );
+};
+
+export default Page;
