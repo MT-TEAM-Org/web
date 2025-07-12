d383bf5f194659a98aedfff1debac32e2095a4da
wlrnjs <tjwlrnjs7336@icloud.com>
2025-07-12 10:09:58 +0900

    refactor: split date elements

---
+++ src/app/(admin)/_components/search/DateElement.tsx
@@ -0,0 +1,29 @@
+import Icon from "@/app/_components/IconComponents";
+import React from "react";
+
+interface DateElementProps {
+  placeholder: string;
+  ref: React.RefObject<HTMLInputElement>;
+}
+
+const DateElement = ({ placeholder, ref }: DateElementProps) => {
+  return (
+    <div className="relative flex flex-1 items-center px-3 py-2">
+      <input
+        type="text"
+        placeholder={placeholder}
+        className="w-full h-[40px] rounded-[5px] p-3 bg-white border border-gray3 text-[14px] leading-[22px] tracking-[-0.02em] cursor-pointer"
+        onClick={() => ref.current?.showPicker()}
+      />
+      <input type="date" ref={ref} className="hidden" />
+      <div
+        className="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer"
+        onClick={() => ref.current?.showPicker()}
+      >
+        <Icon icon="CALENDAR_ICON" />
+      </div>
+    </div>
+  );
+};
+
+export default DateElement;
--- src/app/(admin)/_components/search/DateGroup.tsx
+++ src/app/(admin)/_components/search/DateGroup.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Icon from "@/app/_components/IconComponents";
 import React, { useRef } from "react";
+import DateElement from "./DateElement";
 
 const DateGroup = () => {
   const startRef = useRef<HTMLInputElement>(null);
@@ -14,40 +14,12 @@ const DateGroup = () => {
       </div>
 
       {/* 시작 날짜 */}
-      <div className="relative flex flex-1 items-center px-3 py-2">
-        <input
-          type="text"
-          placeholder="시작 날짜"
-          className="w-full h-[40px] rounded-[5px] p-3 bg-white border border-gray3 text-[14px] leading-[22px] tracking-[-0.02em] cursor-pointer"
-          onClick={() => startRef.current?.showPicker()}
-        />
-        <input type="date" ref={startRef} className="hidden" />
-        <div
-          className="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer"
-          onClick={() => startRef.current?.showPicker()}
-        >
-          <Icon icon="CALENDAR_ICON" />
-        </div>
-      </div>
+      <DateElement placeholder="시작 날짜" ref={startRef} />
       <span className="flex items-center justify-center text-center mx-3 font-normal text-[16px] leading-7 tracking-[-0.02em] text-gray7 select-none">
         ~
       </span>
       {/* 종료 날짜 */}
-      <div className="relative flex flex-1 items-center px-3 py-2">
-        <input
-          type="text"
-          placeholder="종료 날짜"
-          className="w-full h-[40px] rounded-[5px] p-3 bg-white border border-gray3 text-[14px] leading-[22px] tracking-[-0.02em] cursor-pointer"
-          onClick={() => endRef.current?.showPicker()}
-        />
-        <input type="date" ref={endRef} className="hidden" />
-        <div
-          className="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer"
-          onClick={() => endRef.current?.showPicker()}
-        >
-          <Icon icon="CALENDAR_ICON" />
-        </div>
-      </div>
+      <DateElement placeholder="종료 날짜" ref={endRef} />
     </div>
   );
 };
