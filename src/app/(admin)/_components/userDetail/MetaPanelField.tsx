d79a7d719426437889073f7eef51a5ef97f0b17c
wlrnjs <tjwlrnjs7336@icloud.com>
2025-07-12 18:59:30 +0900

    refactor: userDetail refactor

---
+++ src/app/(admin)/_components/userDetail/MetaPanelField.tsx
@@ -0,0 +1,25 @@
+import { cn } from "@/utils";
+
+interface MetaPanelFieldProps {
+  item: any; // 타입 수정 필요
+}
+
+const MetaPanelField = ({ item }: MetaPanelFieldProps) => {
+  return (
+    <div className="w-full min-h-[40px] flex items-center border border-gray2 border-b-0">
+      <p className="w-[100px] h-full px-3 py-2 bg-gray1 flex items-center justify-center font-bold text-[14px] leading-5 text-gray8">
+        {item.name}
+      </p>
+      <p
+        className={cn(
+          "flex-1 px-4 py-2 bg-white h-full font-medium text-[14px] leading-5",
+          item.style
+        )}
+      >
+        {item.value}
+      </p>
+    </div>
+  );
+};
+
+export default MetaPanelField;
