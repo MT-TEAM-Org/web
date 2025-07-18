1507d38e8b88c1d17b21bb41f411b82b4695158c
wlrnjs <tjwlrnjs7336@icloud.com>
2025-07-18 14:06:56 +0900

    feat: 정렬 종류 추가

--- src/app/(admin)/_components/table/DetailTableHeader.tsx
+++ src/app/(admin)/_components/table/DetailTableHeader.tsx
@@ -7,6 +7,7 @@ import {
   TableHeaderItem,
   TableType,
 } from "../../_type/DetailTable/DetailTableHeader";
+import { useRouter, useSearchParams } from "next/navigation";
 
 interface DetailTableHeaderProps {
   type: TableType;
@@ -21,8 +22,52 @@ const DetailTableHeader = ({
   dropDownControl,
   tableConfig,
 }: DetailTableHeaderProps) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const currentSort = searchParams.get("sort");
+  const [currentSortKey, currentSortValue] = currentSort?.split(".") || [];
+
+  const getNextSort = (
+    currentKey: string,
+    currentValue: string | null,
+    sortList: string[],
+    newKey: string
+  ) => {
+    if (currentKey !== newKey) return sortList[0];
+
+    if (!currentValue) return sortList[0];
+
+    const currentIndex = sortList.indexOf(currentValue);
+    if (currentIndex === -1 || currentIndex === sortList.length - 1) {
+      return null;
+    }
+    return sortList[currentIndex + 1];
+  };
+
+  // 정렬 핸들러
+  const handleSort = (key: string, sortList: string[]) => {
+    const params = new URLSearchParams(searchParams);
+
+    // 정렬은 하나만 허용
+    params.delete("sort");
+
+    const nextValue = getNextSort(
+      currentSortKey,
+      currentSortValue,
+      sortList,
+      key
+    );
+
+    if (nextValue) {
+      params.set("sort", `${key}.${nextValue}`);
+    }
+
+    router.push(`?${params.toString()}`, { scroll: false });
+  };
+
   // 드롭다운 핸들러
-  const handleDropDown = (key: string) => {
+  const handleDropDown = (key: string, sortList: string[]) => {
+    handleSort(key, sortList);
     dropDownControl.setDropDown((prev) => ({
       ...prev,
       [key]: !prev[key],
@@ -40,25 +85,25 @@ const DetailTableHeader = ({
         {tableConfig.headers.map((header) => (
           <th
             key={header.key}
-            onClick={() => handleDropDown(header.key)}
+            onClick={() =>
+              handleDropDown(header.key, header.sortValueList || [])
+            }
             className={cn(
               "px-3 py-2 cursor-pointer border-b border-gray2",
-              dropDownControl.dropDown[header.key]
-                ? "bg-bg0"
-                : "hover:bg-gray2",
+              currentSortKey === header.key ? "bg-bg0" : "hover:bg-gray2",
               header.className
             )}
           >
             <div
               className={cn(
                 "flex justify-between items-center font-bold text-[14px] leading-5 select-none",
-                dropDownControl.dropDown[header.key] ? "text-gra" : "text-gray8"
+                currentSortKey === header.key ? "text-gra" : "text-gray8"
               )}
             >
               <span className="mx-auto">{header.label}</span>
               <Icon
                 icon={
-                  dropDownControl.dropDown[header.key]
+                  currentSortKey === header.key
                     ? "SEARCH_DROPDOWN_UP"
                     : "SEARCH_DROPDOWN_DOWN"
                 }
--- src/app/(admin)/_constants/tableHeaders.ts
+++ src/app/(admin)/_constants/tableHeaders.ts
@@ -4,181 +4,220 @@ const getCommonHeaders = (isList: boolean) => ({
     key: "status",
     label: "처리 상태",
     className: "w-[100px]",
+    sortValueList: ["waiting", "completed", "success"]
   },
   nickname: {
     key: "nickname",
     label: "닉네임",
     className: "w-[160px]",
+    sortValueList: ["abc", "desc"],
   },
   content: {
     key: "content",
     label: "내용",
     className: !isList ? "truncate min-w-[103px]" : "truncate flex-1",
+    sortValueList: ["abc", "desc"],
   },
   date: {
     key: "date",
     label: "작성날짜",
     className: "w-[160px]",
+    sortValueList: ["new", "old"],
   },
 });
 
 // 타입별 헤더
 const getTypeSpecificHeaders = (isList: boolean) => ({
+  // 개선요청 관리
   suggestions: {
     importance: {
       key: "importance",
       label: "중요도",
       className: "w-[100px]",
+      sortValueList: ["high", "medium", "low"],
     },
     recommendations: {
       key: "recommendations",
       label: "추천수",
       className: "w-[100px]",
+      sortValueList: ["many", "less"],
     },
     title: {
       key: "title",
       label: "제목",
       className: !isList ? "truncate min-w-[103px]" : "truncate flex-1",
+      sortValueList: ["abc", "desc"],
     },
   },
+  // 문의 관리
   inquiry: {
     member: {
       key: "member",
       label: "회원 여부",
       className: "w-[160px]",
+      sortValueList: ["member", "nonMember"],
     },
     email: {
       key: "email",
       label: "닉네임/이메일",
       className: "w-[160px]",
+      sortValueList: ["abc", "desc"],
     },
   },
+  // 공지 관리
   notice: {
     writer: {
       key: "writer",
       label: "작성자",
       className: "w-[160px]",
+      sortValueList: ["abc", "desc"],
     },
     title: {
       key: "title",
       label: "제목",
       className: "truncate flex-1",
+      sortValueList: ["abc", "desc"],
     },
     content: {
       key: "content",
       label: "내용",
       className: "truncate flex-1",
+      sortValueList: ["abc", "desc"],
     },
   },
+  // 게시글/댓글 관리
   content: {
     isReport: {
       key: "isReport",
       label: "신고 여부",
       className: "w-[100px]",
+      sortValueList: ["report", "notReport"],
     },
     reportCount: {
       key: "reportCount",
       label: "신고수",
       className: "w-[100px]",
+      sortValueList: ["many", "less"],
     },
     userStatus: {
       key: "userStatus",
       label: "회원상태",
       className: "w-[100px]",
+      sortValueList: ["normal", "banned", "warning"]
     },
     writer: {
       key: "writer",
       label: "작성자",
       className: "w-[160px]",
+      sortValueList: ["abc", "desc"],
     },
     type: {
       key: "type",
       label: "유형",
       className: "w-[100px]",
+      sortValueList: ["post", "comment", "chat"],
     },
     titleContent: {
       key: "titleContent",
       label: "제목/내용",
       className: !isList ? "truncate min-w-[103px]" : "truncate flex-1",
+      sortValueList: ["abc", "desc"],
     },
   },
+  // 게시글 상세
   detailContent: {
     reportUser: {
       key: "reportUser",
       label: "신고자",
       className: "w-[160px]",
+      sortValueList: ["abc", "desc"],
     },
     reportType: {
       key: "reportType",
       label: "신고 유형",
       className: "w-[100px]",
+      sortValueList: ["abuse", "explicit", "politics", "promotion", "other"],
     },
     userStatus: {
       key: "userStatus",
       label: "회원상태",
       className: "w-[100px]",
+      sortValueList: ["normal", "banned", "warning"],
     },
     reason: {
       key: "reason",
       label: "사유",
       className: "flex-1 truncate",
+      sortValueList: ["abc", "desc"],
     },
     type: {
       key: "type",
       label: "유형",
       className: "w-[100px]",
+      sortValueList: ["post", "comment", "chat"],
     },
     reportDate: {
       key: "reportDate",
       label: "신고날짜",
       className: "w-[200px]",
+      sortValueList: ["new", "old"],
     },
   },
+  // 회원 관리
   user: {
     userStatus: {
       key: "userStatus",
       label: "회원상태",
       className: "flex-1 truncate",
+      sortValueList: ["waiting", "completed", "success"]
     },
     post: {
       key: "post",
       label: "게시글",
       className: "flex-1 truncate",
+      sortValueList: ["asc", "desc"],
     },
     comment: {
       key: "comment",
       label: "댓글",
       className: "flex-1 truncate",
+      sortValueList: ["many", "less"],
     },
     getRecommendations: {
       key: "getRecommendations",
       label: "받은 추천수",
       className: "flex-1 truncate",
+      sortValueList: ["many", "less"],
     },
     getReportCount: {
       key: "getReportCount",
       label: "받은 신고수",
       className: "flex-1 truncate",
+      sortValueList: ["many", "less"],
     },
     gender: {
       key: "gender",
       label: "성별",
       className: "flex-1 truncate",
+      sortValueList: ["male", "female", "unknown"],
     },
     joinType: {
       key: "joinType",
       label: "가입유형",
       className: "flex-1 truncate",
+      sortValueList: ["google", "discord", "normal"],
     },
     email: {
       key: "email",
       label: "이메일",
       className: "flex-1 truncate",
+      sortValueList: ["asc", "desc"],
     },
     phone: {
       key: "phone",
       label: "휴대폰번호",
       className: "flex-1 truncate",
+      sortValueList: ["filled", "empty"],
     },
   },
   userDetail: {
@@ -186,21 +225,25 @@ const getTypeSpecificHeaders = (isList: boolean) => ({
       key: "reportDate",
       label: "신고날짜",
       className: "w-[160px]",
+      sortValueList: ["old"],
     },
     reportCount: {
       key: "reportCount",
       label: "신고 받은 횟수",
       className: "w-[160px]",
+      sortValueList: ["many", "less"],
     },
     type: {
       key: "type",
       label: "유형",
       className: "w-[100px]",
+      sortValueList: ["post", "comment", "chat"],
     },
     titleContent: {
       key: "titleContent",
       label: "제목/내용",
-      className: "flex-1 truncate"
+      className: "flex-1 truncate",
+      sortValueList: ["abc", "desc"],
     },
   },
 });
