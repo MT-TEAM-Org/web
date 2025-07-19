3fc12d015d0ca081d07f0cefc192e6e21e745c82
wlrnjs <tjwlrnjs7336@icloud.com>
2025-07-20 01:20:12 +0900

    refactor: inputbox 파일 분리

--- src/app/(admin)/dashBoard/my-page/_components/InputBox.tsx
+++ src/app/(admin)/dashBoard/my-page/_components/InputBox.tsx
@@ -1,50 +1,60 @@
+"use client";
+
 import { cn } from "@/utils";
-import React from "react";
+import React, { useState } from "react";
+import { inputFields } from "../_constants/INPUT_FIELDS";
 
 const InputBox = () => {
+  const [form, setForm] = useState({
+    password: "",
+    nickname: "",
+  });
+
   return (
     <>
-      <div className="w-full h-[74px] flex flex-col items-start justify-center gap-1">
-        <p className="font-medium text-[14px] leading-[22px] tracking-[-0.02em] text-gray7">
-          아이디<span className="text-warning">*</span>
-        </p>
-        <input
-          type="text"
-          placeholder="아이디를 입력해주세요"
-          readOnly
-          className="w-full h-[48px] rounded-[5px] border px-4 py-3 text-gray2 border-gray3"
-        />
-      </div>
-      {/* 비밀번호 */}
-      <div className="w-full h-[100px] flex flex-col items-start justify-center gap-1">
-        <p className="font-medium text-[14px] leading-[22px] tracking-[-0.02em] text-gray7">
-          비밀번호<span className="text-warning">*</span>
-        </p>
-        <input
-          type="text"
-          placeholder="비밀번호를 입력해주세요"
-          readOnly
-          className="w-full h-[48px] rounded-[5px] border px-4 py-3 text-gray2 border-gray3"
-        />
-        <p className="px-4 font-medium text-[14px] leading-[22px] tracking-[-0.02em] text-gray5">
-          영문+숫자 조합 4자~10자 이내
-        </p>
-      </div>
-      {/* 닉네임 */}
-      <div className="w-full h-[100px] flex flex-col items-start justify-center gap-1">
-        <p className="font-medium text-[14px] leading-[22px] tracking-[-0.02em] text-gray7">
-          닉네임<span className="text-warning">*</span>
-        </p>
-        <input
-          type="text"
-          placeholder="닉네임을 입력해주세요"
-          readOnly
-          className="w-full h-[48px] rounded-[5px] border px-4 py-3 text-gray2 border-gray3"
-        />
-        <p className="px-4 font-medium text-[14px] leading-[22px] tracking-[-0.02em] text-gray5">
-          한글+영문 / 한글 + 숫자 등 모두 가능 (10자 이내로)
-        </p>
-      </div>
+      {inputFields.map((item) => (
+        <div
+          key={item.name}
+          className={cn(
+            "w-full flex flex-col items-start justify-center gap-1",
+            item.description ? "h-[100px]" : "h-[74px]"
+          )}
+        >
+          <label
+            htmlFor={item.name}
+            className="font-medium text-[14px] leading-[22px] tracking-[-0.02em] text-gray7"
+          >
+            {item.label}
+            <span className="text-warning">*</span>
+          </label>
+
+          <input
+            id={item.name}
+            name={item.name}
+            type={item.type}
+            placeholder={item.placeholder}
+            readOnly={item.readOnly}
+            value={item.readOnly ? item.value : form[item.name]}
+            onChange={
+              item.readOnly
+                ? undefined
+                : (e) => setForm({ ...form, [item.name]: e.target.value })
+            }
+            className={cn(
+              "w-full h-[48px] rounded-[5px] border px-4 py-3 border-gray3",
+              item.readOnly
+                ? "text-gray5 bg-gray2 outline-none focus:outline-none focus:border-none cursor-default select-none"
+                : "text-gray2 bg-white"
+            )}
+          />
+          {/* 설명 */}
+          {item.description && (
+            <p className="px-4 font-medium text-[14px] leading-[22px] tracking-[-0.02em] text-gray5">
+              {item.description}
+            </p>
+          )}
+        </div>
+      ))}
       <button
         className={cn(
           "w-[120px] h-[40px] rounded-[5px] px-4 bg-Primary",
