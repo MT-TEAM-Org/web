fc06d1ab5df8e6a647bf70c7556f6915e5ab4a33
GILPYO <rlfvy789@kakao.com>
2025-07-21 15:12:52 +0900

    pub: 그래프와 탭 레이아웃 퍼블

--- src/app/(admin)/dashBoard/page.tsx
+++ src/app/(admin)/dashBoard/page.tsx
@@ -1,11 +1,18 @@
 import OverviewStatBox from "../_components/statusBanner/OverviewStatBox";
-import StatusBanner from "../_components/statusBanner/StatusBanner";
 
 export default function DashboardPage() {
   return (
-    <div className="w-full">
-      <div className="w-full ">
+    <div className="w-full flex flex-col gap-[40px]">
+      <div className="w-full h-[356px]">
         <OverviewStatBox title="운영 현황" type="dashBoard" />
+      </div>
+      <div className="w-full h-[452px] flex justify-center items-center gap-[40px]">
+        <div className="w-full max-w-[778px] h-[404px] flex justify-center items-center">
+          Graph
+        </div>
+        <div className="w-full max-w-[826px] flex justify-center items-center">
+          Tap
+        </div>
       </div>
     </div>
   );
